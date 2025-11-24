ea9bc3157b2fb571a2f906a3e3d963944b0e633e
Sai Peyyeti <peyyeti.sai@gmail.com>
2025-11-24 19:22:56 +0530

    optimal solution and included infographic

--- lc4/lc4.py
+++ lc4/lc4.py
@@ -1,28 +1,15 @@
 def lengthOfLongestSubstring(s: str) -> int:
-    if len(s) == 0:
-        return 0
+    chMap: dict[char, int] = {}
+    i = 0
+    maxLen = 0
 
-    chMap: dict[str, int] = {}
-    i: int = 0
-    j: int = 0
-    maxLen: int = 1
+    for j, ch in enumerate(s):
+      if ch in chMap and chMap[ch] >= i:
+        i = chMap[ch] + 1
+      chMap[ch] = j
+      maxLen = max(maxLen, j - i + 1)
 
-    chMap[s[0]] = 0
-    j += 1 
-    
-    while j < len(s):
-        if s[j] in chMap:
-            k = chMap[s[j]] + 1
-            while i < k:
-                del chMap[s[i]]
-                i += 1
-        else:
-            if (j - i) + 1 > maxLen:
-                 maxLen = (j - i) + 1
-            chMap[s[j]] = j
-            j += 1
-    
-    return maxLen
+    return maxLen    
          
 print(lengthOfLongestSubstring("abcabcbb"))
 print(lengthOfLongestSubstring("bbbbb"))
