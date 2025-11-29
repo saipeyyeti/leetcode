e50bea6bd89804e0f11100a6603ad5853bbc907d
Sai Peyyeti <peyyeti.sai@gmail.com>
2025-11-29 13:12:21 +0530

    updating the algorithm with optimization

--- lc42/lc42.py
+++ lc42/lc42.py
@@ -1,64 +1,47 @@
-def TrapRainWater(height: list[int]) -> int:
+def trap(height: list[int]) -> int:
+ if not height:
+  return 0
 
- numDict: dict[int, int] = {}
+ i, j = 0, len(height) - 1
+ left_max, right_max = 0, 0
+ water = 0
 
- # loop through left to right and find the places
- cur = 0
- wat = 0
- full = 0
- for i in range(1, len(height)):
-  if height[i] < height[cur]:
-   wat += height[cur] - height[i]
-  else:
-   if wat > 0:
-    numDict[i] = cur
-    full += wat
-    wat = 0
-   cur = i
-
- # loop through right to left and find the places
- cur = len(height) - 1
- wat = 0
- j = cur - 1
- while True:
-  if j < 0:
-   break
-  if cur in numDict:
-   cur = numDict[cur]
-   j = cur - 1
-  else:
-   if height[j] < height[cur]:
-    wat += height[cur] - height[j]
+ while i < j:
+  if height[i] < height[j]:
+   if height[i] >= left_max:
+    left_max = height[i]
    else:
-    if wat > 0:
-     full += wat
-     wat = 0
-    cur = j
+    water += left_max - height[i]
+   i += 1
+  else:
+   if height[j] >= right_max:
+    right_max = height[j]
+   else:
+    water += right_max - height[j]
    j -= 1
-    
-
- return full
  
-print(TrapRainWater([0,1,0,2,1,0,1,3,2,1,2,1]))
-print(TrapRainWater([4,2,0,3,2,5]))
-print(TrapRainWater([0,3,0,1,4,0,2,0,5,1,0,3,2,4,0,2,1,0,3,0,4,2,0,1,5,0,2,3,0,4,1,0]))
-print(TrapRainWater([0]))
-print(TrapRainWater([5]))
-print(TrapRainWater([0,0]))
-print(TrapRainWater([2,2]))
-print(TrapRainWater([1,2,3,4,5]))
-print(TrapRainWater([5,4,3,2,1]))
-print(TrapRainWater([3,0,3]))
-print(TrapRainWater([2,0,2]))
-print(TrapRainWater([2,0,2,0,2]))
-print(TrapRainWater([3,0,1,0,2]))
-print(TrapRainWater([0,0,0,0]))
-print(TrapRainWater([2,2,2,2]))
-print(TrapRainWater([5,0,0,0,5]))
-print(TrapRainWater([5,1,2,1,5]))
-print(TrapRainWater([5,0,0,0,3]))
-print(TrapRainWater([3,0,0,0,5]))
-print(TrapRainWater([0,2,0,2,0,2]))
-print(TrapRainWater([4,1,1,1,4]))
-print(TrapRainWater([100000,0,100000]))
-print(TrapRainWater([0,1,0,2,1,0,1,3,2,1,2,1]))
+ return water
+ 
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+print(trap([4,2,0,3,2,5]))
+print(trap([0,3,0,1,4,0,2,0,5,1,0,3,2,4,0,2,1,0,3,0,4,2,0,1,5,0,2,3,0,4,1,0]))
+print(trap([0]))
+print(trap([5]))
+print(trap([0,0]))
+print(trap([2,2]))
+print(trap([1,2,3,4,5]))
+print(trap([5,4,3,2,1]))
+print(trap([3,0,3]))
+print(trap([2,0,2]))
+print(trap([2,0,2,0,2]))
+print(trap([3,0,1,0,2]))
+print(trap([0,0,0,0]))
+print(trap([2,2,2,2]))
+print(trap([5,0,0,0,5]))
+print(trap([5,1,2,1,5]))
+print(trap([5,0,0,0,3]))
+print(trap([3,0,0,0,5]))
+print(trap([0,2,0,2,0,2]))
+print(trap([4,1,1,1,4]))
+print(trap([100000,0,100000]))
+print(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
